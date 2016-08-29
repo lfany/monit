2675315f2210aa794a85370e03b87d9a40491470
tildeslash <info@tildeslash.com>
2016-08-29 15:29:59 +0200

    Fixed: Monit GUI: Set the encoding to UTF-8 to support UTF-8 formatted "check program" output.

--- src/http/cervlet.c
+++ src/http/cervlet.c
@@ -551,6 +551,7 @@ static void do_head(HttpResponse res, const char *path, const char *name, int re
                             "<meta HTTP-EQUIV='REFRESH' CONTENT=%d> "\
                             "<meta HTTP-EQUIV='Expires' Content=0> "\
                             "<meta HTTP-EQUIV='Pragma' CONTENT='no-cache'> "\
+                            "<meta charset='UTF-8'>" \
                             "<link rel='shortcut icon' href='favicon.ico'>"\
                             "</head>"\
                             "<body><div id='wrap'><div id='main'>" \
