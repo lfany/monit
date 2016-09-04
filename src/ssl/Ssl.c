1c2acd7889429dcd5df81f6ce076bd868a98fbbc
tildeslash <info@tildeslash.com>
2016-09-04 12:42:27 +0200

    Fix Issue #460: Add support for OpenSSL 1.1.0

--- src/ssl/Ssl.c
+++ src/ssl/Ssl.c
@@ -317,12 +317,23 @@ static int _verifyClientCertificates(int preverify_ok, X509_STORE_CTX *ctx) {
                                 return 0;
                 }
         }
+#if (OPENSSL_VERSION_NUMBER < 0x10100000L) || defined(LIBRESSL_VERSION_NUMBER)
         X509_OBJECT found_cert;
         if (X509_STORE_CTX_get_error_depth(ctx) == 0 && X509_STORE_get_by_subject(ctx, X509_LU_X509, X509_get_subject_name(X509_STORE_CTX_get_current_cert(ctx)), &found_cert) != 1) {
+#else
+        X509_OBJECT *found_cert = X509_OBJECT_new();
+        if (X509_STORE_CTX_get_error_depth(ctx) == 0 && X509_STORE_get_by_subject(ctx, X509_LU_X509, X509_get_subject_name(X509_STORE_CTX_get_current_cert(ctx)), found_cert) != 1) {
+#endif
                 LogError("SSL: no matching certificate found -- %s\n", SSLERROR);
                 X509_STORE_CTX_set_error(ctx, X509_V_ERR_CERT_REJECTED);
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+                X509_OBJECT_free(found_cert);
+#endif
                 return 0;
         }
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+        X509_OBJECT_free(found_cert);
+#endif
         return 1;
 }
 
