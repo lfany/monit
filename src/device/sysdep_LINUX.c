91f7e3c388688151ec5c182d6107fdf076505738
tildeslash <info@tildeslash.com>
2017-01-30 11:01:03 +0100

    cleanup

--- src/device/sysdep_LINUX.c
+++ src/device/sysdep_LINUX.c
@@ -65,13 +65,123 @@
 /* ------------------------------------------------------------- Definitions */
 
 
+#define CIFSSTAT "/proc/fs/cifs/Stats"
+#define NFSSTAT  "/proc/self/mountstats"
+
+
 typedef struct Device_T {
         char name[PATH_MAX];
         char type[STRLEN];
+        boolean_t (*getDiskActivity)(struct Device_T *device, Info_T inf);
 } *Device_T;
 
 
 /* ----------------------------------------------------------------- Private */
+
+
+static boolean_t _getCifsDiskActivity(Device_T device, Info_T inf) {
+        FILE *f = fopen(CIFSSTAT, "r");
+        if (! f) {
+                LogError("Cannot open %s file\n", CIFSSTAT);
+                return false;
+        }
+        uint64_t now = Time_milli();
+        char line[PATH_MAX];
+        boolean_t found = false;
+        while (fgets(line, sizeof(line), f)) {
+                if (! found) {
+                        int index;
+                        char name[PATH_MAX];
+                        if (sscanf(line, "%d) %1023s", &index, name) == 2 && IS(name, device->name)) {
+                                found = true;
+                        }
+                } else if (found) {
+                        char label1[256];
+                        char label2[256];
+                        uint64_t operations;
+                        uint64_t bytes;
+                        if (sscanf(line, "%255[^:]: %"PRIu64" %255[^:]: %"PRIu64, label1, &operations, label2, &bytes) == 4) {
+                                if (IS(label1, "Reads") && IS(label2, "Bytes")) {
+                                        Statistics_update(&(inf->priv.filesystem.read.bytes), now, bytes);
+                                        Statistics_update(&(inf->priv.filesystem.read.operations), now, operations);
+                                } else if (IS(label1, "Writes") && IS(label2, "Bytes")) {
+                                        Statistics_update(&(inf->priv.filesystem.write.bytes), now, bytes);
+                                        Statistics_update(&(inf->priv.filesystem.write.operations), now, operations);
+                                        break;
+                                }
+                        }
+                }
+        }
+        fclose(f);
+        return true;
+}
+
+
+static boolean_t _getNfsDiskActivity(Device_T device, Info_T inf) {
+        FILE *f = fopen(NFSSTAT, "r");
+        if (! f) {
+                LogError("Cannot open %s file\n", NFSSTAT);
+                return false;
+        }
+        uint64_t now = Time_milli();
+        char line[PATH_MAX];
+        char pattern[PATH_MAX];
+        boolean_t found = false;
+        snprintf(pattern, sizeof(pattern), "device %s ", device->name);
+        while (fgets(line, sizeof(line), f)) {
+                if (! found && Str_startsWith(line, pattern)) {
+                        found = true;
+                } else if (found) {
+                        char name[256];
+                        uint64_t operations;
+                        uint64_t bytesSent;
+                        uint64_t bytesReceived;
+                        uint64_t time;
+                        if (sscanf(line, " %255[^:]: %"PRIu64" %*u %*u %"PRIu64 " %"PRIu64" %*u %*u %"PRIu64, name, &operations, &bytesSent, &bytesReceived, &time) == 5) {
+                                if (IS(name, "READ")) {
+                                        Statistics_update(&(inf->priv.filesystem.read.time), now, time / 1000.); // us -> ms
+                                        Statistics_update(&(inf->priv.filesystem.read.bytes), now, bytesReceived);
+                                        Statistics_update(&(inf->priv.filesystem.read.operations), now, operations);
+                                } else if (IS(name, "WRITE")) {
+                                        Statistics_update(&(inf->priv.filesystem.write.time), now, time / 1000.); // us -> ms
+                                        Statistics_update(&(inf->priv.filesystem.write.bytes), now, bytesSent);
+                                        Statistics_update(&(inf->priv.filesystem.write.operations), now, operations);
+                                        break;
+                                }
+                        }
+                }
+        }
+        fclose(f);
+        return true;
+}
+
+
+static boolean_t _getBlockDiskActivity(Device_T device, Info_T inf) {
+        char path[PATH_MAX];
+        snprintf(path, sizeof(path), "/sys/class/block/%s/stat", device->name);
+        FILE *f = fopen(path, "r");
+        if (f) {
+                uint64_t now = Time_milli();
+                uint64_t readOperations = 0ULL, readSectors = 0ULL, readTime = 0ULL;
+                uint64_t writeOperations = 0ULL, writeSectors = 0ULL, writeTime = 0ULL;
+                if (fscanf(f, "%"PRIu64" %*u %"PRIu64" %"PRIu64" %"PRIu64" %*u %"PRIu64" %"PRIu64" %*u %*u %*u", &readOperations, &readSectors, &readTime, &writeOperations, &writeSectors, &writeTime) != 6) {
+                        fclose(f);
+                        LogError("filesystem statistic error: cannot parse %s -- %s\n", path, STRERROR);
+                        return false;
+                }
+                Statistics_update(&(inf->priv.filesystem.read.time), now, readTime);
+                Statistics_update(&(inf->priv.filesystem.read.bytes), now, readSectors * 512);
+                Statistics_update(&(inf->priv.filesystem.read.operations), now, readOperations);
+                Statistics_update(&(inf->priv.filesystem.write.time), now, writeTime);
+                Statistics_update(&(inf->priv.filesystem.write.bytes), now, writeSectors * 512);
+                Statistics_update(&(inf->priv.filesystem.write.operations), now, writeOperations);
+                fclose(f);
+        } else {
+                LogError("filesystem statistic error: cannot read %s -- %s\n", path, STRERROR);
+                return false;
+        }
+        return true;
+}
 
 
 static boolean_t _getDevice(char *mountpoint, Device_T device) {
@@ -84,8 +194,13 @@ static boolean_t _getDevice(char *mountpoint, Device_T device) {
         while ((mnt = getmntent(f))) {
                 if (IS(mountpoint, mnt->mnt_dir) && ! IS(mnt->mnt_fsname, "rootfs")) {
                         strncpy(device->type, mnt->mnt_type, sizeof(device->type) - 1);
-                        if (Str_startsWith(mnt->mnt_type, "cifs") || Str_startsWith(mnt->mnt_type, "nfs")) {
+                        if (IS(mnt->mnt_type, "cifs")) {
                                 strncpy(device->name, mnt->mnt_fsname, sizeof(device->name) - 1);
+                                Str_replaceChar(device->name, '/', '\\');
+                                device->getDiskActivity = _getCifsDiskActivity;
+                        } else if (Str_startsWith(mnt->mnt_type, "nfs")) {
+                                strncpy(device->name, mnt->mnt_fsname, sizeof(device->name) - 1);
+                                device->getDiskActivity = _getNfsDiskActivity;
                         } else  {
                                 if (! realpath(mnt->mnt_fsname, device->name)) {
                                         // If the file doesn't exist it's a virtual filesystem -> skip
@@ -94,6 +209,7 @@ static boolean_t _getDevice(char *mountpoint, Device_T device) {
                                         goto error;
                                 }
                                 snprintf(device->name, sizeof(device->name), "%s", File_basename(device->name));
+                                device->getDiskActivity = _getBlockDiskActivity;
                         }
                         endmntent(f);
                         return true;
@@ -107,70 +223,10 @@ error:
 
 
 static boolean_t _getDiskActivity(char *mountpoint, Info_T inf) {
+        boolean_t rv = true;
         struct Device_T device = {};
         if (_getDevice(mountpoint, &device)) {
-                if (Str_startsWith(device.type, "cifs")) {
-                        //FIXME
-                } else if (Str_startsWith(device.type, "nfs")) {
-                        FILE *f = fopen("/proc/self/mountstats", "r");
-                        if (! f) {
-                                LogError("Cannot open /proc/self/mountstats file\n");
-                                return false;
-                        }
-                        uint64_t now = Time_milli();
-                        char line[PATH_MAX];
-                        char pattern[PATH_MAX];
-                        boolean_t found = false;
-                        snprintf(pattern, sizeof(pattern), "device %s ", device.name);
-                        while (fgets(line, sizeof(line), f)) {
-                                if (! found && Str_startsWith(line, pattern)) {
-                                        found = true;
-                                } else if (found) {
-                                        char name[256];
-                                        uint64_t operations;
-                                        uint64_t bytesSent;
-                                        uint64_t bytesReceived;
-                                        uint64_t time;
-                                        if (sscanf(line, " %255[^:]: %"PRIu64" %*u %*u %"PRIu64 " %"PRIu64" %*u %*u %"PRIu64, name, &operations, &bytesSent, &bytesReceived, &time) == 5) {
-                                                if (IS(name, "READ")) {
-                                                        Statistics_update(&(inf->priv.filesystem.read.time), now, time / 1000.); // us -> ms
-                                                        Statistics_update(&(inf->priv.filesystem.read.bytes), now, bytesReceived);
-                                                        Statistics_update(&(inf->priv.filesystem.read.operations), now, operations);
-                                                } else if (IS(name, "WRITE")) {
-                                                        Statistics_update(&(inf->priv.filesystem.write.time), now, time / 1000.); // us -> ms
-                                                        Statistics_update(&(inf->priv.filesystem.write.bytes), now, bytesSent);
-                                                        Statistics_update(&(inf->priv.filesystem.write.operations), now, operations);
-                                                        break;
-                                                }
-                                        }
-                                }
-                        }
-                        fclose(f);
-                } else {
-                        char path[PATH_MAX];
-                        snprintf(path, sizeof(path), "/sys/class/block/%s/stat", device.name);
-                        FILE *f = fopen(path, "r");
-                        if (f) {
-                                uint64_t now = Time_milli();
-                                uint64_t readOperations = 0ULL, readSectors = 0ULL, readTime = 0ULL;
-                                uint64_t writeOperations = 0ULL, writeSectors = 0ULL, writeTime = 0ULL;
-                                if (fscanf(f, "%"PRIu64" %*u %"PRIu64" %"PRIu64" %"PRIu64" %*u %"PRIu64" %"PRIu64" %*u %*u %*u", &readOperations, &readSectors, &readTime, &writeOperations, &writeSectors, &writeTime) != 6) {
-                                        fclose(f);
-                                        LogError("filesystem statistic error: cannot parse %s -- %s\n", path, STRERROR);
-                                        return false;
-                                }
-                                Statistics_update(&(inf->priv.filesystem.read.time), now, readTime);
-                                Statistics_update(&(inf->priv.filesystem.read.bytes), now, readSectors * 512);
-                                Statistics_update(&(inf->priv.filesystem.read.operations), now, readOperations);
-                                Statistics_update(&(inf->priv.filesystem.write.time), now, writeTime);
-                                Statistics_update(&(inf->priv.filesystem.write.bytes), now, writeSectors * 512);
-                                Statistics_update(&(inf->priv.filesystem.write.operations), now, writeOperations);
-                                fclose(f);
-                        } else {
-                                LogError("filesystem statistic error: cannot read %s -- %s\n", path, STRERROR);
-                                return false;
-                        }
-                }
+                rv = device.getDiskActivity(&device, inf);
         } else {
                 Statistics_reset(&(inf->priv.filesystem.read.time));
                 Statistics_reset(&(inf->priv.filesystem.read.bytes));
@@ -179,7 +235,7 @@ static boolean_t _getDiskActivity(char *mountpoint, Info_T inf) {
                 Statistics_reset(&(inf->priv.filesystem.write.bytes));
                 Statistics_reset(&(inf->priv.filesystem.write.operations));
         }
-        return true;
+        return rv;
 }
 
 
