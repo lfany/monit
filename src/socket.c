26d2222a20394e7e37bf5d9c13f0e7b41069bd39
tildeslash <info@tildeslash.com>
2017-03-09 12:22:29 +0100

    Fixed Issue #570 : if the connection test requires STARTTLS, the certificate valid days test was broken.

--- src/socket.c
+++ src/socket.c
@@ -586,10 +586,11 @@ static void _testIp(Port_T p) {
                                 {
                                         S = _createIpSocket(p->hostname, r->ai_addr, r->ai_addrlen, localaddr, p->outgoing.addrlen, r->ai_family, r->ai_socktype, r->ai_protocol, &(p->target.net.ssl.options), p->timeout);
                                         S->Port = p;
+                                        p->protocol->check(S);
 #ifdef HAVE_OPENSSL
+                                        // Set the minimum valid days past the protocol check as if the connection uses STARTTLS to switch plain->SSL, we have no SSL certificate informations until the STARTTTLS is performed
                                         p->target.net.ssl.certificate.validDays = Ssl_getCertificateValidDays(S->ssl);
 #endif
-                                        p->protocol->check(S);
                                         is_available = Connection_Ok;
 
                                 }
