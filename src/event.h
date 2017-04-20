d48ec6669b23c191b8f556fbed697b4580895f9f
tildeslash <info@tildeslash.com>
2017-04-20 17:15:52 +0200

    save comments

--- src/event.h
+++ src/event.h
@@ -28,6 +28,117 @@
 #include "monit.h"
 
 
+//FIXME: could be good to support also event state clasification ... currently we have helper bitmap to differentiate error/change
+//FIXME: keep backward compatibility for M/Monit ... new event types must map to old events and old type reported along new one in XML
+//FIXME: extend alert filters for new event types
+/*
+
+
+typedef struct Events_T {
+        uint8_t action                     : 2;
+        uint8_t checksum                   : 2;
+        uint8_t children                   : 2;
+        uint8_t content                    : 2;
+        uint8_t connection                 : 2;
+        uint8_t cpu                        : 2;
+        uint8_t cpuSystem                  : 2;
+        uint8_t cpuTotal                   : 2;
+        uint8_t cpuUser                    : 2;
+        uint8_t cpuWait                    : 2;
+        uint8_t data                       : 2;
+        uint8_t exec                       : 2;
+        uint8_t exist                      : 2;
+        uint8_t gid                        : 2;
+        uint8_t heartbeat                  : 2;
+        uint8_t instance                   : 2;
+        uint8_t invalid                    : 2;
+        uint8_t filesystemFlags            : 2;
+        uint8_t filesystemInodes           : 2;
+        uint8_t filesystemBytesRead        : 2;
+        uint8_t filesystemBytesWrite       : 2;
+        uint8_t filesystemOperationsRead   : 2;
+        uint8_t filesystemOperationsWrite  : 2;
+        uint8_t filesystemServiceTime      : 2;
+        uint8_t filesystemSpace            : 2;
+        uint8_t linkBytesIn                : 2;
+        uint8_t linkBytesOut               : 2;
+        uint8_t linkError                  : 2;
+        uint8_t linkPacketsIn              : 2;
+        uint8_t linkPacketsOut             : 2;
+        uint8_t linkSaturation             : 2;
+        uint8_t linkSpeed                  : 2;
+        uint8_t linkStatus                 : 2;
+        uint8_t loadAverage1m              : 2;
+        uint8_t loadAverage5m              : 2;
+        uint8_t loadAverage15m             : 2;
+        uint8_t memory                     : 2;
+        uint8_t memoryTotal                : 2;
+        uint8_t nonExist                   : 2;
+        uint8_t permission                 : 2;
+        uint8_t ping                       : 2;
+//        uint8_t processDiskBytesRead       : 2; // use filesystemBytesRead in process content too ... ditto for bellow
+//        uint8_t processDiskBytesWrite      : 2;
+//        uint8_t processDiskOperationsRead  : 2;
+//        uint8_t processDiskOperationsWrite : 2;
+        uint8_t processPid                 : 2;
+        uint8_t processPpid                : 2;
+        uint8_t size                       : 2;
+        uint8_t status                     : 2;
+        uint8_t swap                       : 2;
+        uint8_t threads                    : 2;
+        uint8_t timeout                    : 2;
+        uint8_t timestampAccess            : 2;
+        uint8_t timestampChange            : 2;
+        uint8_t timestampModify            : 2;
+        uint8_t uid                        : 2;
+        uint8_t uptime                     : 2;
+} Events_T;
+
+
+typedef struct EventData_T {
+        boolean_t failed;
+        boolean_t changed;
+} *EventData_T;
+//FIXME: too complicated and fragile?
+union {
+        struct {
+        } directory;
+        struct {
+        } fifo;
+        struct {
+        } file;
+        struct {
+                struct EventData_T space;
+                struct EventData_T inode;
+                struct EventData_T readBytes;
+                struct EventData_T writeBytes;
+        } filesystem;
+        struct {
+        } host;
+        struct {
+        } net;
+        struct {
+                struct EventData_T cpu;
+                struct EventData_T cpuTotal;
+                struct EventData_T memory;
+                struct EventData_T memoryTotal;
+                struct EventData_T uptime;
+        } process;
+        struct {
+        } program;
+        struct {
+                struct EventData_T cpuUser;
+                struct EventData_T cpuSystem;
+                struct EventData_T cpuWait;
+                struct EventData_T memory;
+                struct EventData_T uptime;
+        } system;
+} event;
+*/
+//FIXME: use enum for new Event types, but just normal sequence, not mask ... so we can easily do for example eventCpu??? The Event_post will then internally set the above structure
+//FIXME: keep this for backward compatibility and add new types (covered by sequential enum)?
+//FIXME: add new function that will map new events to old events? ... the Event_post can post the new event and just the XML code will map the event to the old one
+//FIXME: rename?:
 typedef enum {
         Event_Null       = 0x0,
         Event_Checksum   = 0x1,
